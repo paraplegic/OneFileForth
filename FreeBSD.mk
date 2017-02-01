e17f8b23e56b4e8b217700dec734b7eeae15125b
Rob Sciuk <rob@controlq.com>
2017-02-01 17:54:16 -0500

    part deux of directory restructure

--- FreeBSD.mk
+++
@@ -1,5 +0,0 @@
-LDOPTS:=
-CC:=clang
-
-ARMGNU ?= arm-none-eabi
-LDPTH ?= /usr/local/lib/gcc/arm-none-eabi/5.3.0/libgcc.a
--- Linux.mk
+++
@@ -1,5 +0,0 @@
-LDOPTS:=-ldl
-CC:=gcc
-
-ARMGNU ?= arm-linux-gnueabi
-LDPTH ?= /usr/lib/gcc/arm-linux-gnueabi/4.9/libgcc.a
